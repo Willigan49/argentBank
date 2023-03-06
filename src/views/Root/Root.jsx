92a1cdf249d57462bbc76e7ae7cd8a306fe0a257
Guillaume Gernigon <ggernigon@puydufou.com>
2023-03-06 11:19:58 +0100

    footer done

--- src/views/Root/Root.jsx
+++ src/views/Root/Root.jsx
@@ -1,24 +1,29 @@
 import argentBankLogo from "../../assets/argentBankLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function Root() {
   return (
-    <nav class="main-nav">
-      <a class="main-nav-logo" href="./index.html">
-        <img
-          class="main-nav-logo-image"
-          src={argentBankLogo}
-          alt="Argent Bank Logo"
-        />
-        <h1 class="sr-only">Argent Bank</h1>
-      </a>
-      <div className="main-nav-link">
-        <a class="main-nav-item" href="./sign-in.html">
-          <FontAwesomeIcon class="user-icon" icon={solid("user-circle")} />
-          Sign In
+    <div>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src={argentBankLogo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
         </a>
-      </div>
-    </nav>
+        <div className="main-nav-link">
+          <a className="main-nav-item" href="./sign-in.html">
+            <FontAwesomeIcon className="user-icon" icon={solid("user-circle")} />
+            Sign In
+          </a>
+        </div>
+      </nav>
+      <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
+    </div>
   );
 }
