ed20a307cc21a9ac3e1db2fea9815c6212622740
ggernigon <guillaume.gernigon@outlook.fr>
2023-03-21 17:46:40 +0100

    change login to logout button when auth

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
-import store from "../../store";
 import { fetchBearer } from "../../slices/authSlice";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Login() {
   const [userMail, setUserMail] = useState("");
   const [userPassword, setUserPassword] = useState("");
-  const [error, setError] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
-  const state = store.getState();
+  const error = useSelector((state) => state.auth.error);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    setError(state.auth.error);
-  }, [state.auth.error]);
+    setErrorMessage(error);
+  }, [error]);
 
   /**
    * Submit username and password for loging
@@ -20,9 +21,7 @@ export default function Login() {
    */
   async function handleSubmit(event) {
     event.preventDefault();
-    await store.dispatch(
-      fetchBearer({ email: userMail, password: userPassword })
-    );
+    await dispatch(fetchBearer({ email: userMail, password: userPassword }));
     navigate("/profile");
   }
 
@@ -44,22 +43,22 @@ export default function Login() {
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-          {error ? <p className="error">{error}</p> : null}
-          <form onSubmit={handleSubmit}>
-            <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" onChange={handleChange} />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" onChange={handleChange} />
-            </div>
-            <div className="input-remember">
-              <input type="checkbox" id="remember-me" />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
-            <button className="sign-in-button">Sign In</button>
-          </form>
+        {error ? <p className="error">{errorMessage}</p> : null}
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" onChange={handleChange} />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" onChange={handleChange} />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button className="sign-in-button">Sign In</button>
+        </form>
       </section>
     </div>
   );
--- src/views/Root/Root.jsx
+++ src/views/Root/Root.jsx
@@ -1,10 +1,19 @@
 import { Outlet, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import argentBankLogo from "../../assets/argentBankLogo.png";
 
 export default function Root() {
+  const [isAuthenticate, setIsAuthenticate] = useState(false);
+  const token = useSelector((state) => state.auth.accessToken);
+
+  useEffect(() => {
+    token ? setIsAuthenticate(true) : setIsAuthenticate(false);
+  }, [token]);
+
   return (
     <div className="wrapper">
       <nav className="main-nav">
@@ -17,13 +26,23 @@ export default function Root() {
           <h1 className="sr-only">Argent Bank</h1>
         </Link>
         <div className="main-nav-link">
-          <Link className="main-nav-item" to="/profile">
-            <FontAwesomeIcon
-              className="user-icon"
-              icon={solid("user-circle")}
-            />
-            Sign In
-          </Link>
+          {!isAuthenticate ? (
+            <Link className="main-nav-item" to="/profile">
+              <FontAwesomeIcon
+                className="user-icon"
+                icon={solid("user-circle")}
+              />
+              Sign In
+            </Link>
+          ) : (
+            <Link className="main-nav-item" to="/logout">
+              <FontAwesomeIcon
+                className="user-icon"
+                icon={solid("right-from-bracket")}
+              />
+              Sign Out
+            </Link>
+          )}
         </div>
       </nav>
       <main className="main">
